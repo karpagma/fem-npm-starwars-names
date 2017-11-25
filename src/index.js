c5735f4f59bfa72c9f5219bf81f75aa802ff188a
Madhan Ganesh <madhanganesh@gmail.com>
2017-11-25 19:56:36 +0530

    feat(names): added new method getFirst

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import uniqueRandomArray from 'unique-random-array';
 import starWarsNames from './starwars-names.json';
 
+const getFirst = () => starWarsNames[0];
+
 const main = {
   all: starWarsNames,
-  random: uniqueRandomArray(starWarsNames)
+  random: uniqueRandomArray(starWarsNames),
+  getFirst: getFirst
 };
 
 export default main;
